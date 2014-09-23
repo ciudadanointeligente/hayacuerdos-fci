8ec1187683cca3fdb69a91975cae5d66a516182b
J. Pablo Pérez Trabucco <jpereztrabucco@gmail.com>
2014-09-23 12:42:09 -0300

    fixed opositor name in mobile view

--- js/hayacuerdo.js
+++ js/hayacuerdo.js
@@ -1,8 +1,8 @@
 $('#listado').on('change', 'select#lol', function(){
-  
+
   var sheet = $(this).val();
-  
-  Tabletop.init({ 
+
+  Tabletop.init({
                   key: public_spreadsheet_url,
                   callback: function(data, tabletop){
                     var allPositions = $(".allPositions");
@@ -18,7 +18,7 @@ $('#listado').on('change', 'select#lol', function(){
                     });
                     change_name();
                   },
-                  simpleSheet: true 
+                  simpleSheet: true
                 })
 });
 
@@ -33,7 +33,7 @@ function showInfo(data, tabletop) {
   var allPositions = $(".allPositions");
 
   var list = $('<select name="" id="lol"></select>');
-  if ( $(".sheet_page").length ) 
+  if ( $(".sheet_page").length )
     var sheet_page = $(".sheet_page").data('sheet');
   else
     var sheet_page = 'hoja1';
@@ -42,8 +42,8 @@ function showInfo(data, tabletop) {
     sheet_page = url.search.split('=')[1];
 
   var title_compare = $(".title-compare");
-      
-      
+
+
 
   $.each(tabletop.sheets(), function(i, sheet){
     var name = '', active = '';
@@ -51,18 +51,18 @@ function showInfo(data, tabletop) {
       // Example
       case 'government-oposition' :
         name = 'Government - Oposition';
-        if( sheet.name == sheet_page ) 
+        if( sheet.name == sheet_page )
           active = 'selected="selected"';
         break;
       case 'government-students' :
         name = 'Government - Students';
-        if( sheet.name == sheet_page ) 
+        if( sheet.name == sheet_page )
           active = 'selected="selected"';
         break;
       // End example
       case 'gobierno-fech' :
         name = 'Gobierno - FECH';
-        if( sheet.name == sheet_page ) 
+        if( sheet.name == sheet_page )
           active = 'selected="selected"';
         break;
       case 'gobierno-horizontal' :
@@ -142,7 +142,7 @@ function showInfo(data, tabletop) {
     list.append('<option value="'+sheet.name+'" '+active+'>'+name+'</option>');
   });
 
-  list.appendTo('#listado');  
+  list.appendTo('#listado');
 
   $.each(tabletop.sheets(sheet_page).all(), function(index, row){
 
@@ -153,7 +153,7 @@ function showInfo(data, tabletop) {
 }
 
 function drawpositions(index, row, sheet_page) {
-  
+  var opositor_name = $(".title-compare")[0].innerHTML;
   var posture1_twitter = '<a href="#" onclick="window.open(\'https://twitter.com/share?url='+url_share+'%3Fposture='+sheet_page+'&amp;via=ciudadanoi&amp;hashtags=hayacuerdo&amp;text='+row.postura1.substr(0, 80)+'\',\'twitter\',\'width=450, height=250\')"><i class="fa fa-twitter"></i> twitter</a></div>',
       posture2_twitter = '<a href="#" onclick="window.open(\'https://twitter.com/share?url='+url_share+'%3Fposture='+sheet_page+'&amp;via=ciudadanoi&amp;hashtags=hayacuerdo&amp;text='+row.postura2.substr(0, 80)+'\',\'twitter\',\'width=450, height=250\')"><i class="fa fa-twitter"></i> twitter</a></div>';
 
@@ -165,12 +165,12 @@ function drawpositions(index, row, sheet_page) {
 
   view_more_a = '<div class="pull-right"><span><a data-toggle="modal" data-target="#myModal'+index+'a"><i class="fa fa-plus"></i> ver más</a></span>';
   view_more_b = '<div class="pull-right"><span><a data-toggle="modal" data-target="#myModal'+index+'b"><i class="fa fa-plus"></i> ver más</a></span>';
-  
+
   if( row.class === 'noacuerdo' ) {
     posture1 = '<div class="col-md-2 noacuerdo postura1"><p><span class="strong hidden-md hidden-lg">Gobierno: </span>'+row.postura1+view_more_a+posture1_twitter+'</p></div>';
     posture_img = '<div class="col-md-2 hidden-xs noizquierda"><img class="izq" src="../img/noacuerdoizq.svg" alt="lala"></div><div class="col-md-2 col-md-offset-1 text-center texto"><i class="hidden-md hidden-lg fa fa-times fa-3x"></i></div><div class="col-md-2 col-md-offset-1 hidden-xs text-right noderecha"><img class="der" src="../img/noacuerdoder.svg" alt="lala"></div>';
-    posture2 = '<div class="col-md-2 noacuerdo postura2"><p><span class="title-compare strong hidden-md hidden-lg">FECH</span><span class="hidden-md hidden-lg">: </span>'+row.postura2+view_more_b+posture2_twitter+'</p></div>';
-    
+    posture2 = '<div class="col-md-2 noacuerdo postura2"><p><span class="title-compare strong hidden-md hidden-lg">'+opositor_name+'</span><span class="hidden-md hidden-lg">: </span>'+row.postura2+view_more_b+posture2_twitter+'</p></div>';
+
     modal_posture_a = '<div class="modal fade" id="myModal'+index+'a" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4 class="modal-title" id="myModalLabel">'+row.postura1+'</h4></div><div class="modal-body">'+row.postura1txtlargo+'</div><div class="modal-footer"></div></div></div></div>';
     modal_posture_b = '<div class="modal fade" id="myModal'+index+'b" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4 class="modal-title" id="myModalLabel">'+row.postura2+'</h4></div><div class="modal-body">'+row.postura2txtlargo+'</div><div class="modal-footer"></div></div></div></div>';
   }
@@ -178,7 +178,7 @@ function drawpositions(index, row, sheet_page) {
   if( row.class === 'acuerdoparcial' ) {
     posture1 = '<div class="col-md-2 col-md-offset-1 acuerdoparcial postura1"><p><span class="strong hidden-md hidden-lg">Gobierno: </span>'+row.postura1+view_more_a+posture1_twitter+'</p></div>';
     posture_img = '<div class="col-md-2 hidden-xs parcialderecha"><img class="izq" src="../img/acuerdoparcializq.svg" alt="lala"></div><div class="col-md-2 text-center texto"><i class="hidden-sm hidden-md hidden-lg fa fa-exclamation fa-3x"></i></div><div class="col-md-2 hidden-xs text-right parcializquierda"><img class="der" src="../img/acuerdoparcialder.svg" alt="lala"></div>';
-    posture2 = '<div class="col-md-2 acuerdoparcial postura2"><p><span class="title-compare strong hidden-md hidden-lg">FECH</span><span class="hidden-md hidden-lg">: </span>'+row.postura2+view_more_b+posture2_twitter+'</p></div>';
+    posture2 = '<div class="col-md-2 acuerdoparcial postura2"><p><span class="title-compare strong hidden-md hidden-lg">'+opositor_name+'</span><span class="hidden-md hidden-lg">: </span>'+row.postura2+view_more_b+posture2_twitter+'</p></div>';
 
     modal_posture_a = '<div class="modal fade" id="myModal'+index+'a" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4 class="modal-title" id="myModalLabel">'+row.postura1+'</h4></div><div class="modal-body">'+row.postura1txtlargo+'</div><div class="modal-footer"></div></div></div></div>';
     modal_posture_b = '<div class="modal fade" id="myModal'+index+'b" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4 class="modal-title" id="myModalLabel">'+row.postura2+'</h4></div><div class="modal-body">'+row.postura2txtlargo+'</div><div class="modal-footer"></div></div></div></div>';
@@ -187,7 +187,7 @@ function drawpositions(index, row, sheet_page) {
   if( row.class === 'acuerdototal' ) {
     posture1 = '<div class="col-md-2 col-md-offset-3 acuerdototal postura1"><p><span class="strong hidden-md hidden-lg">Gobierno: </span>'+row.postura1+view_more_a+posture1_twitter+'</p></div>';
     posture_img = '<div class="col-md-2 total text-center"><img class="izq hidden-xs hidden-sm" src="../img/acuerdototal.svg" alt="lala"><i class="col-md-2 hidden-sm hidden-md hidden-lg fa fa-check fa-3x"></i></div>';
-    posture2 = '<div class="col-md-2 acuerdototal postura2"><p><span class="title-compare strong hidden-md hidden-lg">FECH</span><span class="hidden-md hidden-lg">: </span>'+row.postura2+view_more_b+posture2_twitter+'</p></div>';
+    posture2 = '<div class="col-md-2 acuerdototal postura2"><p><span class="title-compare strong hidden-md hidden-lg">'+opositor_name+'</span><span class="hidden-md hidden-lg">: </span>'+row.postura2+view_more_b+posture2_twitter+'</p></div>';
 
     modal_posture_a = '<div class="modal fade" id="myModal'+index+'a" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4 class="modal-title" id="myModalLabel">'+row.postura1+'</h4></div><div class="modal-body">'+row.postura1txtlargo+'</div><div class="modal-footer"></div></div></div></div>';
     modal_posture_b = '<div class="modal fade" id="myModal'+index+'b" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4 class="modal-title" id="myModalLabel">'+row.postura2+'</h4></div><div class="modal-body">'+row.postura2txtlargo+'</div><div class="modal-footer"></div></div></div></div>';
